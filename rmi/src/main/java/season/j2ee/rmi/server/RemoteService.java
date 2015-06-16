177a5fdb6ab7b0b0ebc96c8c3eb2e99b7d5373ef
fishblue <boycechou@gmail.com>
2015-06-16 17:23:16 +0800

    根绝PMD给出的修改意见，修改了代码

--- rmi/src/main/java/season/j2ee/rmi/server/RemoteService.java
+++ rmi/src/main/java/season/j2ee/rmi/server/RemoteService.java
@@ -2,7 +2,16 @@ package season.j2ee.rmi.server;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
+/**
+ * 服务器类
+ * @author Administrator
+ *
+ */
 public interface RemoteService extends Remote{
+	/**
+	 * 方法
+	 * @return 字符串
+	 * @throws RemoteException
+	 */
 	public String responseMsg() throws RemoteException;
 }
--- rmi/src/main/java/season/j2ee/rmi/server/RemoteServiceImpl.java
+++ rmi/src/main/java/season/j2ee/rmi/server/RemoteServiceImpl.java
@@ -2,16 +2,27 @@ package season.j2ee.rmi.server;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-
+/**
+ * 
+ * @author Administrator
+ *
+ */
 public class RemoteServiceImpl extends UnicastRemoteObject implements RemoteService{
+	/**
+	 * 序列码
+	 */
+	private static final long serialVersionUID = -5975899476054586317L;
+	/**
+	 * 构造函数
+	 * @throws RemoteException
+	 */
 	protected RemoteServiceImpl() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	/**
-	 * 
+	 * 返回消息
 	 */
-	private static final long serialVersionUID = -5975899476054586317L;
 	public String responseMsg() throws RemoteException{
 		// TODO Auto-generated method stub
 		return "this is a string from remote server!";
